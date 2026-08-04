9ee33f64f2c6dff5746f4360102caa38bf6f14c7
Ritika <ritikanegi1013@gmail.com>
2026-08-04 22:51:28 +0530

    bean scope or instance 04-06-2026

--- src/main/java/com/myapp/demo/firstapp/common/AddressDetails.java
+++ src/main/java/com/myapp/demo/firstapp/common/AddressDetails.java
@@ -1,11 +1,14 @@
 package com.myapp.demo.firstapp.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Lazy //to make class lazy , only creates instance if required
 // marks a class as spring bean and makes it available for dependency injection
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // prototype means different instance for each injection
 public class AddressDetails implements PersonalDetails {
     public AddressDetails(){
         System.out.println("address details");
