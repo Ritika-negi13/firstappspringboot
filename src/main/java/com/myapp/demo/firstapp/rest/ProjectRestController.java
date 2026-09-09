3e53ef056d194885b80f2712500a5bcba45b0d65
Ritika <ritikanegi1013@gmail.com>
2026-09-10 00:11:14 +0530

    created new rest controller and tested it in postman 09-09-2026

---
+++ src/main/java/com/myapp/demo/firstapp/rest/ProjectRestController.java
@@ -0,0 +1,17 @@
+package com.myapp.demo.firstapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class ProjectRestController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello Ritika";
+    }
+
+    //curl --location 'http://localhost:4200/myFirstApp/test/hello'
+}
